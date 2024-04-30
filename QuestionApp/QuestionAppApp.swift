55e56fcf8d9d6875b3078ca81619263f5f227eff
Anne Do <doanneda@gmail.com>
2024-04-30 15:17:33 -0700

    Initial Commit

---
+++ QuestionApp/QuestionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionAppApp.swift
+//  QuestionApp
+//
+//  Created by Scholar on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
